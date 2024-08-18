fa041b98210879d31ec24eca5f5253796ce0fd1a
ceddicedced <cedric@busacker.dev>
2024-08-18 17:37:07 +0000

    feat: Add support for WebAssembly (WASM) and JavaScript (JS) platforms in go-exe-build.sh

--- go-exe-build.sh
+++ go-exe-build.sh
@@ -3,7 +3,7 @@
 
 path=./build
 	
-platforms=("windows/amd64" "windows/386" "linux/amd64" "linux/386" "linux/arm" "linux/arm64")
+platforms=("windows/amd64" "windows/386" "linux/amd64" "linux/386" "linux/arm" "linux/arm64" "wasip1/wasm" "js/wasm")
 
 for platform in "${platforms[@]}"
 do
@@ -13,10 +13,15 @@ do
 	output_name=$path'/'$GOOS'-'$GOARCH
 	if [ $GOOS = "windows" ]; then
 		output_name+='.exe'
+	elif [ $GOARCH = "wasm" ]; then
+		output_name=$path'/'$GOOS'.wasm'
+	else
+		output_name+='.out'
 	fi	
 	env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name .
 	if [ $? -ne 0 ]; then
-   		echo 'An error has occurred! Aborting the script execution...'
-		exit 1
+   		echo 'An error has occurred! '
+		echo 'GOOS='$GOOS
+		echo 'GOARCH='$GOARCH
 	fi
 done
